cb364e2398ce2826013e10b5b95d93a7a5552c0a
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-07-07 02:36:23 +0530

    server successfully setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpStatus } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  try {
+    await app.listen(process.env.PORT ?? 3000);
+    console.log(`Server is running on http://localhost:${3000}/`);
+  } catch (error) {
+    console.error(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    // throw new Error(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }
 bootstrap();

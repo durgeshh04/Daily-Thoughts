f99f796499450c5b6c3494601a54f9593c5cd898
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-07-08 01:48:40 +0530

    Database connection created successfully

---
+++ src/config/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        database: configService.get('POSTGRES_DB'),
+        username: configService.get('POSTGRES_USER'),
+        port: configService.get('POSTGRES_PORT'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   try {
     await app.listen(process.env.PORT ?? 3000);
     console.log(`Server is running on http://localhost:${3000}/`);
+    console.log('Database Connected');
   } catch (error) {
     console.error(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     // throw new Error(error.message, HttpStatus.INTERNAL_SERVER_ERROR);

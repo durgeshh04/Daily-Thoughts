8e01e2d826212324c97b7ce689ebcea28852f113
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-08-29 01:55:48 +0530

    rate limiting added to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

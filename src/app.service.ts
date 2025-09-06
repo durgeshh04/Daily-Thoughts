a83b8baaafb014277a8d8d3aa53834300d3b49ec
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-09-07 02:32:21 +0530

    fix: (monitoring-system) testing sentry error removed from getHello method of App Service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('In the app service');
-    throw new Error('Dummy Error for testing sentry integration');
     return 'Hello World!';
   }
 }

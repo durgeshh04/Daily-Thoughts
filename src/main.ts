679bb65f91de0d336b93026ce9b393dde315974e
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-11-16 22:17:36 +0530

    updated entire application

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,8 @@
-import './sentry/instrument';
-
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import {
-  HttpException,
-  HttpStatus,
-  ValidationPipe,
-  VersioningType,
-} from '@nestjs/common';
-import helmet from 'helmet';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { doubleCsrf } from 'csrf-csrf';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:4000',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
-  });
-
-  app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
-
-  const config = new DocumentBuilder()
-    .setTitle('Daily-Thoughts')
-    .setDescription('api documentation')
-    .setVersion('1.0')
-    // .addTag('')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'jwt',
-        name: 'Authorization',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'access-token',
-    )
-    .build();
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
-  try {
-    await app.listen(process.env.PORT ?? 3000);
-    console.log('Database Connected Successfully', process.env.POSTGRES_DB);
-    console.log(`Server is running on http://localhost:${3000}/api`);
-  } catch (error) {
-    console.error(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-    throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

ae59ac650e6e7e7bd0b996707f9c0ee81327b475
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-12-01 22:52:53 +0530

    dto created for the post request of the user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,34 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(10)
   mobile: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(
+    /^(?=.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    {
+      message:
+        'Wrong password. Must be ≥8 chars, include upper, lower and a number/special char.',
+    },
+  )
+  password: string;
 }

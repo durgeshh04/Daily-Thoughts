d4b299d3eb745bae9669fb51c31cf345b8b978b2
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-12-05 00:55:52 +0530

    validation pipes added

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class Users {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  lastname: string;
+
+  @Column({ type: 'boolean' })
+  active: boolean;
+}

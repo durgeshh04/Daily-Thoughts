bbdec9a1f451be5f5daba92ccbe39790c0c1d432
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-12-14 01:04:10 +0530

    swagger documentation added in the project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,23 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  /**
+   * Swagger Configuration
+   */
+  const config = new DocumentBuilder()
+    .setTitle('Daily-Thoughts Api Documentation')
+    .setDescription('Use the base API URL as http://localhost:3000/')
+    .setTermsOfService('http://localhost:3000/')
+    .setLicense('MIT Licence', 'http://localhost:3000/')
+    .addServer('http://localhost:3000/')
+    .setVersion('1.0')
+    .build();
+
+  // Instantiate document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsNumber()
+  id: number;
+
+  @ApiProperty()
+  @IsString()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  phoneNo: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  address: string;
+}
